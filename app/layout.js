e167016b3c45b9fa5b37281222a9594312cf6313
seversoftech <developersever@gmail.com>
2026-04-07 13:30:08 +0100

    feat: integrate Tailwind CSS v4 and configure VS Code settings

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body suppressHydrationWarning>{children}</body>
